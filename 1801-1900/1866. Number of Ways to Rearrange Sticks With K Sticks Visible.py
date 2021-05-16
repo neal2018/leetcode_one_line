8af10a66189a76b2b0757ab7bc9331a823035ba3
neal2018 <neal.lion@icloud.com>
2021-05-16 21:45:00 +0800

    1866

---
+++ 1801-1900/1866. Number of Ways to Rearrange Sticks With K Sticks Visible.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rearrangeSticks(self, n: int, k: int) -> int:
+        return (lambda n, k: (lambda MOD: (lambda f, n, k: f(f, n, k))(lru_cache(None)(lambda f, n, k: 1 if n == k else 0 if k == 0 else (f(f, n - 1, k - 1) + f(f, n - 1, k) * (n - 1)) % MOD), n, k))(10**9 + 7))(n, k)
+
+
+# explain
+class Solution:
+    def rearrangeSticks(self, n: int, k: int) -> int:
+        return (lambda n, k:
+                (lambda MOD:  # variable MOD to save 10**9 + 7
+                 (lambda f, n, k: f(f, n, k))(  # Y combination to implement recursion
+                     # lru_cache to implement memoization
+                     lru_cache(None)(
+                         lambda f, n, k:  # real recursion function
+                         (   # edge cases
+                             1 if n == k
+                             else 0 if k == 0
+                             # consider the shortest sticks. there is n places we can put it into
+                             # if we put it into the leftest place, it must be seen. hence we decrement k
+                             # if we put it into other n-1 places, it must not be seen. hence k remains
+                             else (f(f, n - 1, k - 1) + f(f, n - 1, k) * (n - 1)) % MOD
+                         )),
+                     n, k)
+                 )(10**9 + 7))(n, k)

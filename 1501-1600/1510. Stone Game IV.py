4c57aa39b8d0c711af8c5588d375affd6995d29a
neal2018 <neal.lion@icloud.com>
2021-05-16 22:10:07 +0800

    1510

---
+++ 1501-1600/1510. Stone Game IV.py
@@ -0,0 +1,15 @@
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        return (lambda n: (lambda f, n: f(f, n))(lru_cache(None)(lambda f, n: any((not f(f, n - i * i)) for i in range(int(sqrt(n)), 0, -1)) if n > 0 else False), n))(n)
+
+
+# explain
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        return (lambda n:
+                (lambda f, n: f(f, n))(  # Y combination to implement recursion
+                    lru_cache(None)(  # cache
+                        lambda f, n:  # real recursion function
+                        # check all possible move and whether exists a situation where opponent must fail
+                        any((not f(f, n - i * i)) for i in range(int(sqrt(n)), 0, -1)) if n > 0 else False
+                    ), n))(n)

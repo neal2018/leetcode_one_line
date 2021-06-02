7d529d0fdd900b340ab52d91e3ae0ba10a7de42a
neal2018 <neal.lion@icloud.com>
2021-06-02 17:37:09 +0800

    591

---
+++ 0500-0600/591. Tag Validator.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+
+class Solution:
+    def isValid(self, code: str) -> bool:
+        return reduce(lambda code, _: re.sub(r'<([A-Z]{1,9})>[^<]*</\1>', '-', code), range(50), re.sub(r'<!\[CDATA\[.*?\]\]>', '-', code)) == "-"
+
+
+# explain
+# regex 50 times
+# you can implement a true while loop, but that requires lots more codes.
+class Solution:
+    def isValid(self, code: str) -> bool:
+        return reduce(lambda code, _:
+                      re.sub(r'<([A-Z]{1,9})>[^<]*</\1>', '-', code),
+                      range(50),
+                      re.sub(r'<!\[CDATA\[.*?\]\]>', '-', code)) == "-"
